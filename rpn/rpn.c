5f21f17a01668a24994e56bec6800247ad7d1c63
Isak <isak@maatta.se>
2023-11-17 16:35:11 +0100

    rpn pog

--- rpn/rpn.c
+++ rpn/rpn.c
@@ -7,69 +7,76 @@
 // Stack data structure
 typedef struct Stack Stack;
 struct Stack {
-    char data[N];
+    int data[N];
     int top;
 };
 
 // Function prototypes
 void initStack(Stack* stack);
 void push(Stack* stack, char operator);
-char pop(Stack* stack);
+int pop(Stack* stack);
+int peek(Stack* stack);
 
 int main(void) {
     Stack stack;
     initStack(&stack);
+
     int a;
     int b;
     int c;
-    int index;
-    index = 0;
+    int line;
+    int ch;
+    line = 0;
+
     while ((c  = getchar()) != EOF) {
         if (c == ' ') {
             continue;
         }
         if (isdigit(c)) {
-            push(&stack, c - '0');
+            int current = c;
+            while ((ch = getchar()) != ' ') {
+                current = current * 10 + (ch - '0');
+            }
+            push(&stack, current - '0');
         }
         else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                a = pop(&stack);
+                b = pop(&stack);
+                int res;
             switch(c) {
                 case '+':
-                    a = pop(&stack);
-                    b = pop(&stack);
-                    push(&stack, a + b);
+                    res = a + b;
                     break;
                 case '-':
-                    a = pop(&stack);
-                    b = pop(&stack);
-                    push(&stack, b - a);
+                    res = b - a;
                     break;
                 case '*':
-                    a = pop(&stack);
-                    b = pop(&stack);
-                    push(&stack, a * b);
+                    res = a * b;
                     break;
                 case '/':
-                    a = pop(&stack);
-                    b = pop(&stack);
                     if (a != 0) {
-                        push(&stack, b / a);
+                        res = b / a;
                     }
                     else {
-                        printf("%s", "Division med noll");
+                        printf("%s\n", "Division med noll ");
+                        continue;
                     }
                     break;
             }
+            printf("%s %d\n", "peek innan vi pushar resultat till stacken", peek(&stack));
+            printf("%s %d\n", "res innan vi pushar resultat till stacken", res);
+            push(&stack, res);
+            printf("%s %d\n", "peek efter vi pushat resultatet till stacken", peek(&stack));
         }
         else if (c == '\n') {
             if (stack.top == -1) {
-                printf("%s\n", "Stack is empty.");
+                printf("%s\n", "Stack is empty. ");
             }
+            printf("line %d %s %d\n", line, ":", pop(&stack));
+            line++;
         } else {
-            printf("%c", c);
             printf("%s\n", "något skumt hände");
-        } 
-        printf("line: %d %d\n", index, (int) stack.top);
-        index++;
+        }
     }
     return 0;
 }
@@ -82,7 +89,7 @@ void initStack(Stack* stack) {
 // Push value onto stack
 void push(Stack* stack, char operator) {
     if (stack->top == N - 1) {
-        printf("Stack is full");
+        printf("%s\n", "Stack is full. i push ");
         return;
     }
     stack->top++;
@@ -90,9 +97,9 @@ void push(Stack* stack, char operator) {
 }
 
 // Pop value from stack
-char pop(Stack* stack) {
+int pop(Stack* stack) {
     if (stack->top == -1) {
-        printf("Stack is empty");
+        printf("%s\n", "Stack is empty. i pop ");
         return -1;
     }
     int value = stack->data[stack->top];
@@ -100,9 +107,9 @@ char pop(Stack* stack) {
     return value;
 }
 
-char peek(Stack *stack) {
+int peek(Stack *stack) {
     if (stack->top == -1) {
-        printf("Stack is empty.");
+        printf("%s\n", "Stack is empty. i peek ");
         return -1;
     }
     return stack->data[stack->top];
